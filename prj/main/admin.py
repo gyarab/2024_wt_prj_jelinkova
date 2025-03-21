c6aa0fb55b8347c4b6198f0b4c9534ce589709f9
Jana Jelínková <jana.jelinkova.s@gyarab.cz>
2025-03-21 12:15:55 +0100

    added tables

--- prj/main/admin.py
+++ prj/main/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Product
+from .models import Category
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "price", "category"]
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
 
 # Register your models here.
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
--- prj/main/models.py
+++ prj/main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=300)
+    price = models.IntegerField(null=True, blank=True)
+    category = models.ForeignKey('Category', null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f"{self.name} ({self.price})"
+    
+class Category(models.Model):
+    name = models.CharField(max_length=300)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return f"{self.name}"
