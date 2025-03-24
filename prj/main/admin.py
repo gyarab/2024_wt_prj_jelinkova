de93265aa19d1d25208cc47280e61f567bf69330
JanaJelinkova <jana.jelinkova.s@gyarab.cz>
2025-03-24 18:02:24 +0100

    added user

--- prj/main/admin.py
+++ prj/main/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Product
 from .models import Category
 from .models import Review
+from .models import User
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ["id", "name", "price", "category"]
@@ -10,9 +11,13 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ["id", "name"]
 
 class ReviewAdmin(admin.ModelAdmin):
-    list_display = ["id", "content", "product"]
+    list_display = ["id", "content", "product", "user"]
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
 
 # Register your models here.
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Review, ReviewAdmin)
+admin.site.register(User, UserAdmin)
--- prj/main/models.py
+++ prj/main/models.py
@@ -20,6 +20,13 @@ class Category(models.Model):
 class Review(models.Model):
     content = models.TextField(max_length=400)
     product = models.ForeignKey('Product', null=True, on_delete=models.SET_NULL)
+    user = models.ForeignKey('User', null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return f"{self.content} ({self.product})"
+
+class User(models.Model):
+    name = models.CharField(max_length=300)
+
+    def __str__(self):
+        return f"{self.name}"
