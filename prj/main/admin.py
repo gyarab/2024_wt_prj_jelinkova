181c26b5688689df178d96f2088fdf667c2e55c4
Jana Jelínková <jana.jelinkova.s@gyarab.cz>
2025-03-28 12:15:27 +0100

    added ProductDiet

--- prj/main/admin.py
+++ prj/main/admin.py
@@ -3,6 +3,8 @@ from .models import Product
 from .models import Category
 from .models import Review
 from .models import User
+from .models import Diet
+from .models import ProductDiet
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ["id", "name", "price", "category"]
@@ -11,13 +13,21 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ["id", "name"]
 
 class ReviewAdmin(admin.ModelAdmin):
-    list_display = ["id", "content", "product", "user"]
+    list_display = ["id", "content", "product", "user", "stars"]
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ["id", "name"]
+
+class DietAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
+
+class ProductDietAdmin(admin.ModelAdmin):
+    list_display = ["id", "product", "diet"]
 
 # Register your models here.
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Review, ReviewAdmin)
 admin.site.register(User, UserAdmin)
+admin.site.register(Diet, DietAdmin)
+admin.site.register(ProductDiet, ProductDietAdmin)
--- prj/main/models.py
+++ prj/main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Product(models.Model):
     name = models.CharField(max_length=300)
@@ -19,14 +20,34 @@ class Category(models.Model):
     
 class Review(models.Model):
     content = models.TextField(max_length=400)
-    product = models.ForeignKey('Product', null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey('Product', null=True, on_delete=models.CASCADE)
     user = models.ForeignKey('User', null=True, on_delete=models.SET_NULL)
+    stars = models.IntegerField(
+    null=True,
+    validators=[
+        MinValueValidator(1),
+        MaxValueValidator(5),
+    ]
+)
 
     def __str__(self):
-        return f"{self.content} ({self.product})"
+        return f"{self.content} ({self.stars})"
 
 class User(models.Model):
     name = models.CharField(max_length=300)
 
     def __str__(self):
         return f"{self.name}"
+    
+class Diet(models.Model):
+    name = models.CharField(max_length=300)
+
+    def __str__(self):
+        return f"{self.name}"
+    
+class ProductDiet(models.Model):
+    product = models.ForeignKey('Product', null=True, on_delete=models.CASCADE)
+    diet = models.ForeignKey('Diet', null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.product}  ({self.diet})"
